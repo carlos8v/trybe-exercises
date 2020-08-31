7042eb82c1a22f1a9f2e5aeedbcf673ef4dcadd1
Carlos Souza <carlos.pessoal@hotmail.com>
2020-08-31 11:17:26 -0300

    Sétimo exercício do bloco 4, dia 2

--- exercises/4.2/exercicio5.js
+++ exercises/4.2/exercicio5.js
@@ -1,8 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let highestNumber = 0;
+let highestNumber;
 
 for (let i = 0; i < numbers.length; i++) {
+    if (i === 0) {
+        highestNumber = numbers[i];
+    }
+
     if (numbers[i] > highestNumber) {
         highestNumber = numbers[i];
     }
