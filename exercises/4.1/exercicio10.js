3a1072706c83cc47f743f1fc9fbd6853872b7c2e
Carlos Souza <carlos.pessoal@hotmail.com>
2020-08-31 10:16:16 -0300

    Corrigido um erro com o cálculo do imposto

--- exercises/4.1/exercicio10.js
+++ exercises/4.1/exercicio10.js
@@ -1,5 +1,5 @@
 const cost = parseFloat((Math.random() * 101).toFixed(2));
-const totalCost = parseFloat((cost + (cost * 20 / 100).toFixed(2)));
+const totalCost = parseFloat(((cost + (cost * 20 / 100)).toFixed(2)));
 const price = parseFloat((Math.random() * 201).toFixed(2));
 
 const profit = price - totalCost;
