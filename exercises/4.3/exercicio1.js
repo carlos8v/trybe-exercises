d05a0c184371c4e90a22182cdb186a8e737b01c9
Carlos Souza <carlos.pessoal@hotmail.com>
2020-09-01 21:04:24 -0300

    Primeiro exercício do bloco 4, dia 3

---
+++ exercises/4.3/exercicio1.js
@@ -0,0 +1,10 @@
+const n = Math.ceil(Math.random() * 10 + 1);
+console.log(`Valor de n: ${n}`);
+
+for (let i = 0; i < n; i++) {
+    let text = '';
+    for (let j = 0; j < n; j++) {
+        text += "*";
+    }
+    console.log(text);
+}
