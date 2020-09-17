4539cdb0b2a37ea58abea357756793b6cc42b28f
Carlos Souza <carlos.pessoal@hotmail.com>
2020-09-17 19:17:54 -0300

    Primeiro fieldset do exercício do bloco 6, dia 1

---
+++ exercises/6_1/script.js
@@ -0,0 +1,43 @@
+function initializeStates() {
+    const select = document.querySelector('select');
+    const states = {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    }
+
+    for (let key in states) {
+        const option = document.createElement('option');
+        console.log(option);
+        option.value = key;
+        option.innerText = states[key];
+        select.appendChild(option);
+    }
+}
+document.body.onload = function () {
+    initializeStates();
+}
