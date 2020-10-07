6755a3c10588a22bee9427cafc71eff9d6614a45
Carlos Souza <carlos.pessoal@hotmail.com>
2020-10-07 18:29:01 -0300

    Primeiro exercício do bloco 8, dia 3

---
+++ exercises/higher-order-functions-do-javascript-es6_3/exercise1.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+function flatten() {
+	return arrays.reduce((acc, curr) => {
+		curr.forEach((item) => acc.push(item));
+		return acc;
+	}, []);
+}
+
+assert.deepEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
