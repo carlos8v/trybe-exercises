7f0c9273c46a552e0cdc970b259a9bf233b7c60e
Carlos Souza <carlos.pessoal@hotmail.com>
2020-10-30 20:21:22 -0300

    Sexto exercício do bloco 10, dia 3

--- exercises/testes-automatizados-com-jest_3/exerciseFunctions.js
+++ exercises/testes-automatizados-com-jest_3/exerciseFunctions.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const randomNumber = () => Math.floor(Math.random() * 100 + 1);
 
 const firstFunction = (str) => str.toUpperCase();
@@ -6,9 +8,16 @@ const secondFunction = (str) =>  str.charAt(0);
 
 const thirdFunction = (str1, str2) => str1.concat(str2);
 
+const fetchDog = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random');
+  const data = await response.json();
+  return data;
+}
+
 module.exports = {
   randomNumber,
   firstFunction,
   secondFunction,
   thirdFunction,
+  fetchDog,
 };
