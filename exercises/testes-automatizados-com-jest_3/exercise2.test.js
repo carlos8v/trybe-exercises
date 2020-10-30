a9140cdc7a41cc033222a0f6327f2f352bf6fa8a
Carlos Souza <carlos.pessoal@hotmail.com>
2020-10-30 19:45:13 -0300

    Terceiro exercício do bloco 10, dia 3

--- exercises/testes-automatizados-com-jest_3/exercise2.test.js
+++ exercises/testes-automatizados-com-jest_3/exercise2.test.js
@@ -1,11 +1,12 @@
 const service = require('./exerciseFunctions');
 
 describe('randomNumber', () => {
-  it('should return an default number', () => {
+  it('should return a division between two numbers', () => {
     service.randomNumber = jest.fn().mockImplementation((a, b) => {
       if (b === 0) return 'Second argument should not be 0';
       return a / b;
     });
+    
     expect(service.randomNumber(10, 2)).toBe(5);
     expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
